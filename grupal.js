00bdd3e9dc74e306fdb69d1403a068638bd83fe6
santimichelena <68866518+santimichelena@users.noreply.github.com>
2020-07-29 14:00:45 -0300

    tarea
    tarea

---
+++ grupal.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+alert("Pronto agregaremos nueva información");
+let funcion = document.getElementById("parrafo");
+funcion.innerHTML = "Somos una empresa con amplia experiencia en el sector de IT, actualmente brinda servicios con especial foco en el área de Diseño Web y Marketing Digital. Utilizando modernas herramientas tecnológicas y estándares de seguridad informática promueve soluciones  a medida para Pequeñas y Medianas empresas inmersas en la transformación digital apoyando el diseño de su marca, identidad visual y el posicionamiento web en internet.";
+
+
+
+
+});
